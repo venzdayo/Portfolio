4cf99d1c134d7bad35d46bd415406feade96a412
comradeZnev <venzmilado@gmail.com>
2025-09-07 23:52:36 +0800

    Initial commit React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center text-white">
+      <h1 className="text-4xl font-handwriting">Under Construction</h1>
+    </div>
+  )
+}
+
+export default App;
+
